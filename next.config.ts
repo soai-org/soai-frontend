a51082b92d8e10c7ffede724e2044537bbd1b736
Duhoon <eclip6@naver.com>
2025-08-21 08:51:33 +0900

    fix: module not found 해결

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
