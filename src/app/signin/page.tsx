a430f4037b45c9fe9cd5614724123d290a0f766f
Duhoon <eclip6@naver.com>
2025-08-27 17:51:10 +0900

    feat: 로그인 페이지 예외 처리 UI 추가

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,12 +11,16 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useState } from "react";
 import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("test@example.com");
-  const [password, setPassword] = useState("password");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const params = useSearchParams();
+
+  const [failed, setFailed] = useState(false);
 
   const handleLogin = async () => {
     await signIn("credentials", {
@@ -24,6 +29,12 @@ export default function LoginPage() {
       callbackUrl: "/",
     });
   };
+
+  useEffect(() => {
+    if (params.has("error")) {
+      setFailed(true);
+    }
+  }, [params, setFailed]);
 
   return (
     <main className="flex items-center bg-background justify-center min-h-screen">
@@ -35,6 +46,7 @@ export default function LoginPage() {
           <div className="grid gap-2">
             <Label htmlFor="email">ID</Label>
             <Input
+              className={cn(failed && "border-red-500")}
               id="id"
               type="string"
               placeholder="ID를 입력하세요."
@@ -46,12 +58,21 @@ export default function LoginPage() {
           <div className="grid gap-2">
             <Label htmlFor="password">패스워드</Label>
             <Input
+              className={cn(failed && "border-red-500")}
               id="password"
               type="password"
+              placeholder="*******"
               required
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
+          </div>
+          <div className="min-h-[10px]">
+            {failed ? (
+              <span className="text-red-500">로그인에 실패했습니다.</span>
+            ) : (
+              <></>
+            )}
           </div>
         </CardContent>
         <CardFooter>
